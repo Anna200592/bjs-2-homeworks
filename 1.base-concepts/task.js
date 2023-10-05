469573607865c2c66a881b1f6fa116b0a297be52
Neto test <strokova-92@mail.ru>
2023-10-05 19:52:20 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+	const dscrt = b ** 2 - 4 * a * c;
+
+	if (dscrt < 0) {
+		return [];
+	} else if (dscrt === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const root1 = (-b + Math.sqrt(dscrt)) / (2 * a);
+		const root2 = (-b - Math.sqrt(dscrt)) / (2 * a);
+		return [root1, root2];
+	}
 }
+function calculateTotalMortgage(percent, contribution, 
+  amount, countMonths) {
+    if (
+      typeof percent !== 'number' ||
+      typeof contribution !== 'number' ||
+      typeof amount !== 'number' ||
+      typeof countMonths !== 'number'
+    ) {
+      return false;
+    }
+    percent = percent / 100 / 12;
+    const loanBody = amount - contribution;
+    const monthlyPayment =
+      loanBody *
+      (percent +
+        percent / (Math.pow(1 + percent, countMonths) - 1));
+    const totalAmount = monthlyPayment * countMonths;
+  
+    return +totalAmount.toFixed(2);
+  }
