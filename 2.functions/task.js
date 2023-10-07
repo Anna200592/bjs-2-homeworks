65073b3cf05ba9f51800d05a574a32c41e03cee7
Neto test <strokova-92@mail.ru>
2023-10-07 12:11:04 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -61,6 +61,10 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
   
+  if (arr.length === 0) {
+		return 0;
+	}
+  
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumEvenElement += arr[i];
