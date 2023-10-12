2b465a72bd38e8f48b7a1a01f16e13afe7a9ef5f
Neto test <strokova-92@mail.ru>
2023-10-12 12:58:53 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
-}
 
-function getUsersNamesInAgeRange(users, gender) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return arr1.every((element, index) => element === arr2[index]);
+  }
+
+  function getUsersNamesInAgeRange(users, gender) {
   
-}
+    const filteredUsers = users.filter(user => user.gender === gender);
+  
+    const ages = filteredUsers.map(user => user.age);
+  
+    const averageAge = ages.reduce((sum, age) => sum + age, 0) / ages.length;
+    
+    return averageAge;
+  }
