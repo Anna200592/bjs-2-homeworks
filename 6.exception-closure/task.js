23a177c5db39bcc37ff4eeca522bfea5923dbf38
Neto test <strokova-92@mail.ru>
2023-10-19 13:10:11 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿"use strict"
+
+function parseCount (val){
+    const result = parseFloat(val);
+    if (isNaN(result)){
+        throw new Error("Невалидное значение")
+    }
+    return result;
+}
+
+function validateCount(val){
+    try{
+        return parseCount(val);
+    } catch(i){
+        return i;
+    }
+}
+
+class Triangle{
+    constructor(a,b,c){
+        if (a + b < c || a + c < b || c + b < a){
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c
+    }
+
+    get area(){
+        const p = this.perimeter / 2;
+        return +Math.sqrt(p * (p - this.a) * (p - this.b)* (p - this.c)).toFixed(3);
+    }
+}
+
+   
+function getTriangle (a,b,c) {
+    try {
+        return new Triangle(a, b, c)
+    } catch (i){
+        return{
+            get perimeter(){
+                return "Ошибка! Треугольник не существует";
+            },
+            
+            get area(){
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
